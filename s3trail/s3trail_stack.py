0215d73e0721b5bb9c525cf411e1972652bfde99
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2022-09-12 20:13:52 -0400

    initial commit

--- s3trail/s3trail_stack.py
+++ s3trail/s3trail_stack.py
@@ -1,19 +1,79 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    RemovalPolicy,
+    aws_s3 as s3,
+    aws_cloudtrail as cloudtrail,
+    aws_iam as iam,
+    aws_kms as kms,
+    aws_logs as logs,
 )
 from constructs import Construct
 
-class S3TrailStack(Stack):
 
+class S3TrailStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        permissions_boundary_policy_arn = self.node.try_get_context(
+            "PermissionsBoundaryPolicyArn"
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "S3TrailQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        if not permissions_boundary_policy_arn:
+            permissions_boundary_policy_name = self.node.try_get_context(
+                "PermissionsBoundaryPolicyName"
+            )
+            if permissions_boundary_policy_name:
+                permissions_boundary_policy_arn = self.format_arn(
+                    service="iam",
+                    region="",
+                    account=self.account,
+                    resource="policy",
+                    resource_name=permissions_boundary_policy_name,
+                )
+                print(permissions_boundary_policy_arn)
+
+        if permissions_boundary_policy_arn:
+            policy = iam.ManagedPolicy.from_managed_policy_arn(
+                self, "PermissionsBoundary", permissions_boundary_policy_arn
+            )
+            iam.PermissionsBoundary.of(self).apply(policy)
+
+        # if a KMS key name is provided, enable bucket encryption
+        kms_key_alias = self.node.try_get_context("KmsKeyAlias")
+        if kms_key_alias:
+            kms_params = {
+                "encryption": s3.BucketEncryption.KMS,
+                "bucket_key_enabled": True,
+                "encryption_key": kms.Key.from_lookup(
+                    self, "KmsS3Key", alias_name=kms_key_alias
+                ),
+            }
+        else:
+            kms_params = {}
+
+        audited_bucket = s3.Bucket(
+            self,
+            "Inbound",
+            auto_delete_objects=True,
+            removal_policy=RemovalPolicy.DESTROY,
+            event_bridge_enabled=True,
+            **kms_params
+        )
+
+        s3trail = cloudtrail.Trail(
+            self,
+            "s3trail",
+            # encryption_key
+            cloud_watch_logs_retention=logs.RetentionDays.ONE_YEAR,
+        )
+        s3trail.add_s3_event_selector(
+            [
+                cloudtrail.S3EventSelector(
+                    bucket=audited_bucket,
+                    # object_prefix
+                )
+            ],
+            # exclude_management_event_sources = [],
+            include_management_events=True,
+        )
