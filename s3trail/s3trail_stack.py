0933679c735d37b10a0571a8de65c8c3aefcbba9
Patrick M. Ryan <patrick.ryan@evanschambers.com>
2022-09-16 14:26:10 -0400

    this one also deploys

--- s3trail/s3trail_stack.py
+++ s3trail/s3trail_stack.py
@@ -40,19 +40,30 @@ class S3TrailStack(Stack):
             iam.PermissionsBoundary.of(self).apply(policy)
 
         # if a KMS key name is provided, enable bucket encryption
-        kms_key_alias = self.node.try_get_context("KmsKeyAlias")
-        if kms_key_alias:
-            kms_key = kms.Key.from_lookup(self, "KmsS3Key", alias_name=kms_key_alias)
-            kms_params = {
-                "encryption": s3.BucketEncryption.KMS,
-                "bucket_key_enabled": True,
-                "encryption_key": kms_key,
-            }
-        else:
-            kms_key = None
-            kms_params = {}
 
-        logs_bucket = s3.Bucket(self, "LogsBucket", **kms_params)
+        # kms_key_alias = self.node.try_get_context("KmsKeyAlias")
+        # if kms_key_alias:
+        #     kms_key = kms.Key.from_lookup(self, "KmsS3Key", alias_name=kms_key_alias)
+        #     kms_params = {
+        #         "encryption": s3.BucketEncryption.KMS,
+        #         "bucket_key_enabled": True,
+        #         "encryption_key": kms_key,
+        #     }
+        # else:
+        #     kms_key = None
+        #     kms_params = {}
+
+        trail_key = kms.Key(
+            self,
+            "CloudtrailKey",
+            enable_key_rotation=True,
+            # removal_policy=RemovalPolicy.DESTROY,
+        )
+        kms_params = {
+            "encryption": s3.BucketEncryption.KMS,
+            "bucket_key_enabled": True,
+            "encryption_key": trail_key,
+        }
 
         audited_bucket = s3.Bucket(
             self,
@@ -60,56 +71,40 @@ class S3TrailStack(Stack):
             auto_delete_objects=True,
             removal_policy=RemovalPolicy.DESTROY,
             event_bridge_enabled=True,
-            **kms_params,
-            # server_access_logs_bucket=logs_bucket,
-            # server_access_logs_prefix="AuditedBucket",
+            # **kms_params,
         )
 
-        # logs_bucket.add_to_resource_policy(
-        #     iam.PolicyStatement(
-        #         principals=[iam.ServicePrincipal("logging.s3.amazonaws.com")],
-        #         actions=["s3:PutObject"],
-        #         resources=[logs_bucket.arn_for_objects('*')]
-        #     )
-        # )
-        # logs_bucket.grant_put(iam.ServicePrincipal("logging.s3.amazonaws.com"))
+        trail_key.grant_encrypt_decrypt(
+            iam.ServicePrincipal(
+                "cloudtrail.amazonaws.com",
+                # conditions={"StringEquals": {"aws:SourceArn": s3trail.trail_arn}},
+            )
+        )
 
-        trail = self.trail_for_bucket(bucket=audited_bucket, key=kms_key)
-
-        # audited_bucket.grant_put(iam.ServicePrincipal("cloudtrail.amazonaws.com"))
-
-    def trail_for_bucket(self, bucket, key):
-
-        # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_cloudtrail/Trail.html
         s3trail = cloudtrail.Trail(
             self,
             "s3trail",
             # send_to_cloud_watch_logs=True,
             # cloud_watch_logs_retention=logs.RetentionDays.ONE_YEAR,
             #   have to grant kms access to a principal
-            encryption_key=key,
+            encryption_key=trail_key,
         )
 
-        if key:
-            key.grant_encrypt_decrypt(
-                iam.ServicePrincipal(
-                    "cloudtrail.amazonaws.com",
-                    conditions={"StringEquals": {"aws:SourceArn": s3trail.trail_arn}},
-                )
-            )
-
-        # s3trail.log_all_s3_data_events
+        # kms_key.grant_encrypt_decrypt(
+        #     iam.ServicePrincipal(
+        #         "cloudtrail.amazonaws.com",
+        #         conditions={"StringEquals": {"aws:SourceArn": s3trail.trail_arn}},
+        #     )
+        # )
 
         # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_cloudtrail/S3EventSelector.html#aws_cdk.aws_cloudtrail.S3EventSelector
         s3trail.add_s3_event_selector(
             [
                 cloudtrail.S3EventSelector(
-                    bucket=bucket,
+                    bucket=audited_bucket,
                     # object_prefix
                 )
             ],
             # exclude_management_event_sources = [],
             include_management_events=False,
         )
-
-        return s3trail
